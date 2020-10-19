ced3b5170e98d738cf8dfa99e2ea5208b31dee94
Arkoprobho Chatterjee <arko1999@gmail.com>
2020-10-19 12:37:07 +0530

    Add module for board setup and move-making, factory funtion for players

---
+++ main.js
@@ -0,0 +1,49 @@
+function Player(name, sym)
+{
+	return {name, sym};
+}
+
+let gameBoard = (function (){
+	let matrix = [];
+	let players = [];
+	let currentPlayer = 0;
+	let setupBoard = function(){
+		console.log("In setupBoard...");
+		for(let i = 0; i < 3; i++)
+		{
+			matrix.push(["", "", ""]);
+		}
+		let p1 = Player("Player 1", "X");
+		let p2 = Player("Player 2", "O");
+		//console.log({p1, p2});
+		players.push(p1);
+		players.push(p2);
+		currentPlayer = 0;
+		// add event listeners
+		gridCells = document.querySelectorAll(".grid-cell");
+		gridCells.forEach(cell => {
+			cell.addEventListener("click", makeMove);
+			cell.textContent = "";
+			cell.disabled = false;
+		});
+	}
+	// TODO: add win-checking function
+	let makeMove = function()
+	{
+		// is event listener for the buttons
+		// on click, checks current player, draws symbol, disables
+		console.log("makeMove called");
+		let sym = players[currentPlayer].sym;
+		this.textContent = sym;
+		let x = Number(this.value.substring(0,1));
+		let y = Number(this.value.substring(1));
+		matrix[x][y] = sym;
+		currentPlayer = (currentPlayer + 1) % 2;
+		this.disabled = true;
+	}
+
+	return {setupBoard: setupBoard};
+})();
+
+let ngButton = document.getElementById("rst");
+ngButton.onclick = gameBoard.setupBoard;
